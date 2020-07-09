1f261a07c2115495f86f6644489f2b2f9f88ae12
Stephen <farrenzo47@gmail.com>
2020-07-08 21:46:01 -0400

    Add api key to user association

---
+++ src/migrations/20200709013748-add-associations.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.addColumn('ApiKeys', 'userId', {
+      type: Sequelize.INTEGER,
+      references: {
+        model: 'Users',
+        key: 'id',
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.removeColumn('ApiKeys', 'userId');
+  }
+};
