36118af467e7958a249934d30dc74f667df8aa89
Stephen <farrenzo47@gmail.com>
2020-07-17 10:59:14 -0400

    Get api user info from bearer token

--- src/controllers/ApiController.js
+++ src/controllers/ApiController.js
@@ -1,12 +1,30 @@
 
 const { UnauthorizedRequest } = require('../errors');
 const ApiKeyService = require('../services/ApiKeyService');
+const UserService = require('../services/UserService');
 
 module.exports = class ApiController {
     static async validateBearerToken(bearerToken) {
         if (!bearerToken) {
             throw new UnauthorizedRequest('Must pass valid bearer token in authorization header');
         }
+        const { publicKey, privateKey } = this._extractKeysFromBearerToken(bearerToken);
+        if (!(publicKey && privateKey)) {
+            throw new UnauthorizedRequest('Must pass valid bearer token in authorization header');
+        }
+
+        // make sure key exists
+        const apiKey = await ApiKeyService.getByKeys(publicKey, privateKey);
+    }
+
+    static async getApiUserFromBearerToken(token) {
+        const { publicKey, privateKey } = this._extractKeysFromBearerToken(token);
+        const apiKey = await ApiKeyService.getByKeys(publicKey, privateKey);
+        const userId = apiKey['UserId'];
+        return await UserService.getById(userId);
+    }
+
+    static _extractKeysFromBearerToken(bearerToken) {
         // take out "Bearer" piece
         const token = bearerToken.split('Bearer').pop();
 
@@ -16,11 +34,6 @@ module.exports = class ApiController {
         const keyArr = bearerTokenStringDecoded.split(':');
         const publicKey = keyArr[0] ? keyArr[0].trim() : null;
         const privateKey = keyArr[1] ? keyArr[1].trim() : null;
-        if (!(publicKey && privateKey)) {
-            throw new UnauthorizedRequest('Must pass valid bearer token in authorization header');
-        }
-
-        // make sure key exists
-        const apiKey = await ApiKeyService.getByKeys(publicKey, privateKey);
+        return { publicKey, privateKey };
     }
 }
